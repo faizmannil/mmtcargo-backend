bf7425ee3334af317e9138d743b55b4ee30a6307
faizmannil <muhammedfaizm2@gmail.com>
2025-05-09 02:37:46 +0400

    Added working Flask backend

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-Placeholder content for app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to MMT Cargo Backend!"
+
+if __name__ == '__main__':
+    app.run()
