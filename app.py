d7207cf1d9c18ebb9c87626699b4dbc1f91f32dc
faizmannil <muhammedfaizm2@gmail.com>
2025-05-09 14:14:03 +0400

    Connected frontend templates to Flask backend

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Welcome to MMT Cargo Backend!"
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/sales')
+def sales():
+    return render_template("sales.html")
+
+@app.route('/income')
+def income():
+    return render_template("income.html")
+
+@app.route('/expenses')
+def expenses():
+    return render_template("expenses.html")
+
+@app.route('/withdrawals')
+def withdrawals():
+    return render_template("withdrawals.html")
 
 if __name__ == '__main__':
     app.run()
